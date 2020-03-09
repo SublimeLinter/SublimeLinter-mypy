d5e91e5c8a7c629852f936ac13722789bfdcba28
herr kaste <herr.kaste@gmail.com>
2020-03-09 21:27:10 +0100

    Keep recent caches on startup
    The caches are *very* expensive to compute so we should not trash them on
    every start of Sublime.
    
    We of course trash everything if the plugin gets uninstalled, otherwise
    we clean if the last access time (`st_atime`) is older than 14 days.
    
    A couple of tricks here:
    
    - The `except NameError` trick keeps the cache warm if SublimeLinter
      (or this plugin) reloads. Otherwise Python would clean up the
      directories.
    
    - If you exit Sublime, it is always a fast exit. (Sublime wants to be
      fast!) And so the temporary directories are kept. However, on a cold
      load `tmpdirs` is obviously empty, we thus need to repopulate it.
    
      Unfortunately we cannot just pass strings around because Python would
      trash every `TemporaryDirectory` immediateley on garbage collection.
      So we still store these objects, and resurrected directories become
      `FakeTemporaryDirectory` just for the interface of having a `.name`.

--- linter.py
+++ linter.py
@@ -11,14 +11,26 @@
 
 """This module exports the Mypy plugin class."""
 
+import hashlib
 import logging
 import os
 import shutil
 import tempfile
+import time
 import getpass
 
 from SublimeLinter.lint import const
 from SublimeLinter.lint import PythonLinter
+from SublimeLinter.lint.linter import PermanentError
+
+
+MYPY = False
+if MYPY:
+    from typing import Dict, Protocol
+
+    class TemporaryDirectory(Protocol):
+        name = None  # type: str
+
 
 USER = getpass.getuser()
 TMPDIR_PREFIX = "SublimeLinter-contrib-mypy-%s" % USER
@@ -28,7 +40,10 @@ logger = logging.getLogger("SublimeLinter.plugin.mypy")
 # Mapping for our created temporary directories.
 # For smarter caching purposes,
 # we index different cache folders based on the working dir.
-tmpdirs = {}
+try:
+    tmpdirs
+except NameError:
+    tmpdirs = {}  # type: Dict[str, TemporaryDirectory]
 
 
 class Mypy(PythonLinter):
@@ -80,37 +95,87 @@ class Mypy(PythonLinter):
         # by not littering everything with `.mypy_cache` folders.
         if not self.settings.get('cache-dir'):
             cwd = self.get_working_dir()
-            if cwd in tmpdirs:
-                cache_dir = tmpdirs[cwd].name
+            if not cwd:  # abort silently
+                self.notify_unassign()
+                raise PermanentError()
+
+            if os.path.exists(os.path.join(cwd, '.mypy_cache')):
+                self.settings.set('cache-dir', False)  # do not set it as arg
             else:
-                tmp_dir = tempfile.TemporaryDirectory(prefix=TMPDIR_PREFIX)
-                tmpdirs[cwd] = tmp_dir
-                cache_dir = tmp_dir.name
-                logger.info("Created temporary cache dir at: %s", cache_dir)
-            cmd[1:1] = ["--cache-dir", cache_dir]
+                # Add a temporary cache dir to the command if none was specified.
+                # Helps keep the environment clean by not littering everything
+                # with `.mypy_cache` folders.
+                try:
+                    cache_dir = tmpdirs[cwd].name
+                except KeyError:
+                    tmpdirs[cwd] = tmp_dir = _get_tmpdir(cwd)
+                    cache_dir = tmp_dir.name
+
+                self.settings.set('cache-dir', cache_dir)
 
         return cmd
 
 
-def _cleanup_tmpdirs():
+class FakeTemporaryDirectory:
+    def __init__(self, name):
+        # type: (str) -> None
+        self.name = name
+
+
+def _get_tmpdir(folder):
+    # type: (str) -> TemporaryDirectory
+    folder_hash = hashlib.sha256(folder.encode('utf-8')).hexdigest()[:7]
+    tmpdir = tempfile.gettempdir()
+    for dirname in os.listdir(tmpdir):
+        if dirname.startswith(TMPDIR_PREFIX) and dirname.endswith(folder_hash):
+            path = os.path.join(tmpdir, dirname)
+            tmp_dir = FakeTemporaryDirectory(path)  # type: TemporaryDirectory
+            try:  # touch it so `_cleanup_tmpdirs` doesn't catch it
+                os.utime(path)
+            except OSError:
+                pass
+            logger.info("Reuse temporary cache dir at: %s", path)
+            return tmp_dir
+    else:
+        tmp_dir = tempfile.TemporaryDirectory(prefix=TMPDIR_PREFIX, suffix=folder_hash)
+        logger.info("Created temporary cache dir at: %s", tmp_dir.name)
+        return tmp_dir
+
+
+def _cleanup_tmpdirs(keep_recent=False):
+
     def _onerror(function, path, exc_info):
         logger.exception("Unable to delete '%s' while cleaning up temporary directory", path,
                          exc_info=exc_info)
+
     tmpdir = tempfile.gettempdir()
     for dirname in os.listdir(tmpdir):
         if dirname.startswith(TMPDIR_PREFIX):
-            shutil.rmtree(os.path.join(tmpdir, dirname), onerror=_onerror)
+            full_path = os.path.join(tmpdir, dirname)
+            if keep_recent:
+                try:
+                    atime = os.stat(full_path).st_atime
+                except OSError:
+                    pass
+                else:
+                    if (time.time() - atime) / 60 / 60 / 24 < 14:
+                        continue
+
+            shutil.rmtree(full_path, onerror=_onerror)
 
 
 def plugin_loaded():
     """Attempt to clean up temporary directories from previous runs."""
-    _cleanup_tmpdirs()
+    _cleanup_tmpdirs(keep_recent=True)
 
 
 def plugin_unloaded():
-    """Clear references to TemporaryDirectory instances.
+    try:
+        from package_control import events
 
-    They should then be removed automatically.
-    """
-    # (Actually, do we even need to do this?)
-    tmpdirs.clear()
+        if events.remove('SublimeLinter-contrib-mypy'):
+            logger.info("Cleanup temporary directories.")
+            _cleanup_tmpdirs()
+
+    except ImportError:
+        pass
